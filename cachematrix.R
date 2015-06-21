030253e99bc7b6e202a5eb20d14f63f6c126ce7a
newyeeliu <yliu67@buffalo.edu>
2015-06-21 16:20:39 -0400

    do the assignmet2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,20 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cacheMatrix <- makeVector(x) # call the makeVector accessory function
+  im <- x$getInverse # get the Inverse attribute from the "getter" (i.e. "get" function)
+  if (!is.null(im)) {
+    message("Getting the cached inverseMatrix")
+    return(im) # if the inverse already exists, get the inverse from the cache
+  } else {
+    inverseMatrix <- cacheSolve(x) # if the inverse does not yet exist, cacheSolve will return the inverse
+  }
+  setinverse <- function(cacheMatrix) cacheSolve(cacheMatrix) #"setter"
+  getInverse <- function(cacheMatrix) cacheMatrix #"getter"
+  list(set = set, get = get, # create storage for resulting S3 objects
+       setInverse = setInverse,
+       getInverse = getInverse)
+  return(cacheMatrix)
 }
 
 
@@ -12,4 +25,6 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverseMatrix <- solve(x) #solve returns the inverse of a matrix when given an invertible matrix as input (we assume that the input is an invertible matrix in this assignment)
+  return(inverseMatrix) #explicitly returning the return value with the return function is slower though perhaps clearer, cleaner code.
 }
